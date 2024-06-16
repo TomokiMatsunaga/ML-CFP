b3e111f3d4c9cc652e7102f9fafd8c3ac4f181b0
TomokiMatsunaga <T.Matsunaga@keio.jp>
2024-06-16 14:54:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from MLCFP import extract_evaluation, extraction_detail_eval
 
 if __name__ == '__main__':
     extract_evaluation('MAPS/ENSTDkCl/MUS/*.wav', 'MAPS/ENSTDkCl/MUS/',
-                       dataset='MAPS', instrument_wise=1, instlist=[1], short=30)
+                       dataset='MAPS', instrument_wise=1, instlist=[1], length=30)
     extraction_detail_eval('temp/comp/*.npz', 'temp/label/', 'temp/detail_eval.csv')
 
 
